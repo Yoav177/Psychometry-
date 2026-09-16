1f71fae552c70a622a70d0ead7c6eca457a00591
Yoav177 <markmanyoav@gmail.com>
2026-09-17 00:03:17 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "psy-vocab-v3";
+const FILES = ["./", "index.html", "manifest.webmanifest", "icon-180.png", "icon-192.png", "icon-512.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then(hit => {
+    const net = fetch(e.request).then(res => {
+      if (res && (res.ok || res.type === "opaque")) { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); }
+      return res;
+    }).catch(() => hit);
+    return hit || net;
+  }));
+});
